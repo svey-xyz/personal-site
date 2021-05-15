310bc185a1ec744bca2140bc2172c98b065977f6
soulelyhayden <haydensoule@outlook.com>
2021-05-15 15:11:15 -0400

    Restructuring CSS files to match Tailwinds structure

--- config/postcss.config.js
+++ config/postcss.config.js
@@ -7,8 +7,14 @@ module.exports = {
 	plugins: [
 		require('postcss-import-ext-glob'),
 		require('postcss-import'),
+		// require('postcss-mixins'), // might be useful later
 		require('tailwindcss')('./src/style/tailwind.config.js'),
-		require('postcss-nesting'),
+		require('postcss-preset-env')({
+			features: {
+				'nesting-rules': true,
+				'custom-media-queries': true
+			}
+		}),
 		require('autoprefixer'),	
 		...(process.env.NODE_ENV === 'production' ? [cssnano] : []),
 	],
--- src/style/tailwind.config.js
+++ src/style/tailwind.config.js
@@ -10,10 +10,10 @@ module.exports = {
 	},
 	theme: {
 		screens: {
-			sm: '480px',
-			md: '768px',
-			lg: '976px',
-			xl: '1440px',
+			sm: "480px",
+			md: "768px",
+			lg: "976px",
+			xl: "1440px",
 		},
 		colors: {
 			white: colors.white,
