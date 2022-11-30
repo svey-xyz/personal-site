5e1303df7c53a54cb1b6542326ea397d43d390ee
soulelyhayden <haydensoule@outlook.com>
2022-11-29 19:12:54 -0500

    vldx npm publish

--- src/_includes/ts/components/blocks/contact/contact.ts
+++ src/_includes/ts/components/blocks/contact/contact.ts
@@ -1,5 +1,5 @@
-import formValidator from '../../../../../../plugins/node-form-validator'
+import vldx from 'vldx';
 
 export const mount = (container: HTMLElement) => {
-	let validator = new formValidator(container as HTMLFormElement, '6LfhR1ohAAAAABJFXPocs9NyudnQgvZNDH1DtQ3c')
+	let validator = new vldx();
 }
