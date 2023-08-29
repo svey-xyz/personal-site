ff1685fc814693801d5cf59f3bea2b09b5c0cd49
svey <x@svey.xyz>
2023-08-29 00:09:45 -0400

    Video object schema

--- sanityStudio/schemas/objects/video.ts
+++ sanityStudio/schemas/objects/video.ts
@@ -12,25 +12,28 @@ export const video = defineType({
 			name: 'title',
 			type: 'string',
 			description: 'Title of the video.'
-			// validation: Rule => Rule.required()
 		}),
 		defineField({
 			title: 'Description',
 			name: 'description',
 			type: 'string',
 			description: 'A brief description of the embed, this could be the content, the medium, etc.'
-			// validation: Rule => Rule.required()
 		}),
+		defineField({
+			title: 'Video',
+			name: 'video',
+			type: 'mux.video',
+		})
 	],
 	preview: {
 		select: {
 			title: 'title',
-			url: 'url'
+			desc: 'description'
 		},
 		prepare(value: any) {
 			return {
-				title: value.title ? value.title : 'Video',
-				subtitle: `URL: ${value.url}`,
+				title: value.title ? value.title : 'Untitled Video',
+				subtitle: value.desc ? value.desc : `no description`,
 				media: RiFilmFill
 			}
 		}
