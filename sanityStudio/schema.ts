5f33964a1e7eded7d1eced7615a93f931027754e
svey <x@svey.xyz>
2023-09-05 16:23:48 -0400

    Project construct as basic document

--- sanityStudio/schema.ts
+++ sanityStudio/schema.ts
@@ -1,3 +1,13 @@
+/**
+ * Settings
+ */
+import { navigation } from '@schemas/settings/navigation'
+import { siteSettings } from '@schemas/settings/siteSettings'
+import { theme } from '@schemas/settings/theme'
+import { mux } from '@schemas/settings/mux.api'
+
+const _settings = [navigation, siteSettings, theme, mux]
+
 /** 
  * Objects
  */
@@ -5,23 +15,22 @@ import { basicBlockContent, extraBlockContent } from '@schemas/objects/blockCont
 import { link } from '@schemas/objects/link'
 import { social } from '@schemas/objects/social'
 import { textObject } from '@schemas/objects/text'
+import { projectObjectsList } from '@schemas/documents/project'
+
 import { gallery } from '@schemas/objects/gallery'
 import { video } from '@schemas/objects/video'
 import { basicDate } from '@schemas/objects/basicDate'
 import { basicDocumentOptions } from '@/sanityStudio/lib/basicDocumentOptions'
 
-const _objects = [basicBlockContent, extraBlockContent, link, social, textObject, gallery, video, basicDate, basicDocumentOptions]
+const _objects = [basicBlockContent, extraBlockContent, link, social, textObject, gallery, video, basicDate, basicDocumentOptions, projectObjectsList]
 
 /**
  * Documents
  */
-import { navigation } from '@schemas/settings/navigation'
-import { siteSettings } from '@schemas/settings/siteSettings'
-import { theme } from '@schemas/settings/theme'
-import { project, projectObjects } from '@schemas/documents/project'
+
+import { project } from '@schemas/documents/project'
 import { taxonomicTerm } from '@schemas/documents/taxonomy'
-import { mux } from '@schemas/settings/mux.api'
 
-const _documents = [siteSettings, navigation, theme, project, taxonomicTerm, mux]
+const _documents = [project, taxonomicTerm]
 
-export const types = [..._documents, ..._objects, projectObjects];
+export const types = [..._settings, ..._documents, ..._objects];
--- sanityStudio/schemas/documents/project.ts
+++ sanityStudio/schemas/documents/project.ts
@@ -1,98 +1,26 @@
 import { defineType, defineField, defineArrayMember } from "@sanity-typed/types";
-import { mediaAssetSource } from "sanity-plugin-media";
+import { basicDocumentConstructor } from "@/sanityStudio/lib/basicDocumentConstructor";
 
-import { AiFillFileImage } from 'react-icons/ai';
+import { BsFillBookmarkFill } from 'react-icons/bs'
 
 import { textObject } from '@schemas/objects/text'
 import { gallery } from '@schemas/objects/gallery'
 import { video } from '@schemas/objects/video'
 
-export const projectObjects = defineType({
-	title: 'Project Items',
-	name: 'projectObjects',
+export const _projectObjects = [textObject, gallery, video]
+export const projectObjectsList = defineType({
+	name: 'projectObjectsList',
 	type: 'array',
 	of: [
 		defineArrayMember({ type: textObject.name }),
 		defineArrayMember({ type: gallery.name }),
 		defineArrayMember({ type: video.name }),
 	],
-
 })
 
-export const project = defineType({
-	title: "Project",
-	name: "project",
-	type: 'document',
-	icon: AiFillFileImage,
-	groups: [
-		{
-			name: 'projectSettings',
-			title: 'Project Settings',
-		},
-		{
-			name: 'projectContent',
-			title: 'Project Content',
-		},
-	],
-	fields: [
-		defineField({
-			title: 'Title',
-			name: 'title',
-			type: 'string',
-			group: 'projectSettings',
-			description: 'Title of the project.',
-			validation: (Rule) => [
-				Rule.required().error("Project needs a title!"),
-			]
-		}),
-		defineField({
-			name: 'slug',
-			title: 'Slug',
-			type: 'slug',
-			options: {
-				source: 'title',
-				maxLength: 96,
-			},
-		}),
-		defineField({
-			title: 'Thumbnail',
-			name: 'thumbnail',
-			type: 'image',
-			options: {
-				sources: [mediaAssetSource]
-			},
-			group: 'projectSettings',
-			description: 'Thumbnail for the project, this does not appear in the project content unless you add the image there as well.',
-			validation: Rule => Rule.required()
-		}),
-		defineField({
-			title: 'Date',
-			name: 'date',
-			type: 'date',
-			group: 'projectSettings',
-			validation: (Rule) => [
-				Rule.required().warning("Projects should have a date!"),
-			]
-		}),
-		defineField({
-			title: 'Project Description',
-			name: 'description',
-			type: 'blockContent',
-			group: 'projectSettings',
-			description: 'A description of the project.',
-		}),
-		defineField({
-			title: 'Tags',
-			name: 'tags',
-			type: 'array',
-			group: 'projectSettings',
-			of: [
-				defineArrayMember({
-					type: 'reference',
-					to: [{ type: 'taxonomicTerm' }]
-				})
-			]
-		}),
+export const project = basicDocumentConstructor({
+	name: 'project',
+	contentFields: [
 		defineField({
 			title: 'Github',
 			name: 'github',
@@ -106,51 +34,10 @@ export const project = defineType({
 			description: 'The main GitHub repo for the project. If there are additional repos add them to the external links',
 		}),
 		defineField({
-			title: 'Links',
-			name: 'links',
-			type: 'array',
-			group: 'projectSettings',
-			of: [
-				defineArrayMember({
-					type: 'link'
-				})
-			],
-			description: 'All relevant links for the project.'
-		}),
-		defineField({
 			title: 'Content',
 			name: 'content',
-			type: 'projectObjects',
-			group: 'projectContent',
+			type: 'projectObjectsList',
 			description: 'The main content of the project.',
-		})
-	],
-	preview: {
-		select: {
-			title: 'title',
-			thumbnail: 'thumbnail'
-		},
-		prepare(value: any) {
-			return {
-				title: value.title,
-				media: value.thumbnail
-			}
-		}
-	},
-	orderings: [
-		{
-			title: 'Date Descending',
-			name: 'dateDesc',
-			by: [
-				{ field: 'date', direction: 'desc' }
-			]
-		},
-		{
-			title: 'Date Ascending',
-			name: 'dateAsc',
-			by: [
-				{ field: 'date', direction: 'asc' }
-			]
-		},
-	]
-});
+		})],
+	icon: BsFillBookmarkFill
+})
