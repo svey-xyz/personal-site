4a0bc8e894eb25cbb792b9505332f2cd5cdf6876
svey <x@svey.xyz>
2023-09-11 16:09:23 -0400

    Shadow styles between light/dark

--- app/_components/site/Header.tsx
+++ app/_components/site/Header.tsx
@@ -20,7 +20,7 @@ export default function Header({
 
 function headerWrapper({children}:{children: React.ReactNode}) {
 	return (
-		<nav className='fixed top-0 left-0 right-0 max-w-screen z-50 shadow-xl'>
+		<nav className='fixed top-0 left-0 right-0 max-w-screen z-50 shadow-lg'>
 			{children}
 		</nav>
 	)
--- app/_components/site/TextBlock.tsx
+++ app/_components/site/TextBlock.tsx
@@ -4,7 +4,7 @@ export function TextBlock({
 	children: React.ReactNode
 }): React.ReactNode {
 	return (
-		<div className='relative w-full min-w-full shadow-inner-xdark
+		<div className='relative w-full min-w-full shadow-inner
 			after:absolute after:inset-0 after:-z-1 after:bg-primary-bg after:bg-gradient-to-br after:from-primary-bg after:to-secondary-bg'>
 			<div className="flex flex-col items-center max-w-prose-full py-32 mx-auto">
 				{children}
