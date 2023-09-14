20af764fa310a2516d39e986f2cc15e70ec4b9d0
svey <x@svey.xyz>
2023-09-14 14:50:07 -0400

    Icon animation

--- app/_components/site/Icon.tsx
+++ app/_components/site/Icon.tsx
@@ -8,11 +8,12 @@ export function Icon() {
 	return (
 		<div aria-label={`Site Icon`}
 			className="group cursor-pointer relative flex items-center justify-center w-icon h-icon" >
-			<Icon_Z className="absolute min-h-full min-w-full fill-[#837da1] drop-shadow-md" />
-			<Icon_X className="absolute min-h-full min-w-full fill-[#333] drop-shadow-md" />
-			<Icon_Y className="absolute min-h-full min-w-full fill-[#d0b6c8] drop-shadow-md" />
-
-
+			<Icon_Z className="absolute min-h-full min-w-full fill-[#837da1] drop-shadow-md
+				duration-300 transition-transform group-hover:rotate-6 group-hover:translate-x-12" />
+			<Icon_X className="absolute min-h-full min-w-full fill-[#333] dark:fill-[#eee] drop-shadow-md
+				duration-300 transition-transform group-hover:rotate-0" />
+			<Icon_Y className="absolute min-h-full min-w-full fill-[#d0b6c8] drop-shadow-md
+				duration-300 transition-transform group-hover:-rotate-6 group-hover:translate-x-6" />
 		</div>
 	)
 }
