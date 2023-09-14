abf3ca0cbbb0f24cf37f7bd50fb5498d6c057ae4
svey <x@svey.xyz>
2023-09-13 21:17:18 -0400

    Header & menu styling

--- app/_components/site/MenuModal.tsx
+++ app/_components/site/MenuModal.tsx
@@ -9,12 +9,12 @@ export async function MenuModal({ componentParams }: { componentParams: componen
 	const settings: settingsData = await client.fetch(settingsQuery)
 
 	return (
-		<div className="mt-[--total-header-height] absolute inset-0 w-full z-20 peer-checked:flex hidden md:main-padding
+		<div className="mt-[--total-header-height] absolute inset-0 w-full peer-checked:flex hidden md:main-padding
 			after:fixed after:inset-0 after:bg-primary-accent after:-z-10 after:mt-[75px] after:opacity-70">
 
 			<div className="flex flex-col h-screen w-full bg-gradient-to-br from-primary-bg to-secondary-bg shadow-md 
 					md:mt-12 md:flex-row md:h-fit md:bg-gradient-to-r md:rounded-xl md:shadow-lg">
-				<div className="block border-b-2 px-6 py-6 sm:px-12 md:px-6 lg:px-12 md:border-b-0 md:border-r-2 border-secondary-accent z-10">
+				<div className="block border-b-2 px-6 py-6 sm:px-12 md:px-6 lg:px-12 md:border-b-0 md:border-r-2 border-secondary-accent">
 					<div className="relative block w-12 md:w-16">
 
 						<SanityImage
