804adf3dca86dcacc57756ac0398d319058817df
svey <x@svey.xyz>
2023-11-28 20:22:36 +0000

    Better client repo list rendering

--- components/RepoList.tsx
+++ components/RepoList.tsx
@@ -29,49 +29,29 @@ export function RepoList({ repos }: { repos: Array<singleRepoData> }) {
 		sortArray(sortType);
 	}, [sortType]);
 
-	if (!mounted) return RepoSortedSection({repos})
-
 	return (
 		<div className="max-w-prose">
 			<div className="flex flex-row justify-between mb-4">
 				<h2>projects</h2>
 				<select className='p-2 cursor-pointer border border-bg-primary/40 rounded
 				backdrop-blur-xl bg-bg/70 hover:bg-bg/40 transition-colors duration-300' 
-					onChange={(e) => setSortType(e.target.value)}>
+					onChange={(e) => { if (mounted) setSortType(e.target.value)}}>
 					<option value="created_at">Created</option>
 					<option value="pushed_at">Pushed</option>
 				</select>
 			</div>
 
-			<div className="flex flex-col gap-2">
-				{ repoData.map((repo) => {
-					if (!repo) return
-					return (
-						<ProjectCard key={repo.id} repo={(repo as singleRepoData)} />
-					)
-				}) }
-			</div>
-		</div>
-	)
-}
-
-function RepoSortedSection({ repos }: { repos: Array<singleRepoData> }) {
-	return (
-		<div className="max-w-prose">
-			<div>
-				<h2>projects</h2>
-				<select>
-					<option value="created_at">Created</option>
-				</select>
-			</div>
-
-			<div className="flex flex-col gap-2">
-				{repos.map((repo) => {
-					if (!repo) return
-					return (
-						<ProjectCard key={repo.id} repo={(repo as singleRepoData)} />
-					)
-				})}
+			<div className="flex flex-col gap-1">
+				{(
+					(() => {
+						const repoList = mounted ? repoData : repos
+						return repoList.map((repo) => {
+							return (
+								<ProjectCard key={repo.id} repo={(repo as singleRepoData)} />
+							)
+						})
+					})()
+				)}
 			</div>
 		</div>
 	)
