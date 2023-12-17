99361c707d8b30d5e9239f67b47026bb07a6308f
svey <x@svey.xyz>
2023-12-17 22:45:59 +0000

    Closes #11
    Project info sidebar

--- app/(site)/projects/[slug]/page.tsx
+++ app/(site)/projects/[slug]/page.tsx
@@ -1,9 +1,13 @@
-import React from "react";
-import { ProjectData } from "@lib/data";
+import React, { ReactElement } from "react";
+import { ProjectData, UserData } from "@lib/data";
+import { IoGitMerge } from "react-icons/io5";
 
 import { MarkdownRenderer } from "@components/MarkdownRenderer";
 import NoteCard from "@components/NoteCard";
 import dateConverter from "@lib/dateConverter";
+import { FaLink, FaGithub } from "react-icons/fa6";
+import { taxonomy } from "@/lib/types/data.types";
+import Icon from "@/components/Icon";
 
 export default async function ProjectPage({ params }: { params: { slug: string } }) {
 	const slugs = ProjectData.projects.map((project) => {
@@ -17,28 +21,88 @@ export default async function ProjectPage({ params }: { params: { slug: string }
 		<div className="after:inset-0 after:absolute after:-z-1 after:backdrop-blur-2xl after:bg-gradient-to-b
 			after:from-bg/50 after:to-bg after:to-75% dark:after:from-bg/70 dark:after:to-bg">		
 			<div className="relative main-padding flex flex-col">
-				<div className="max-w-prose">
-					{/* PROJECT TITLE */}
-					<a href='/projects' aria-label='Return to projects archive.' className="relative block w-full h-full py-4">
-						{`<- ${project.title }`}
-					</a>
+				{/* PROJECT TITLE */}
+				<a href='/projects' aria-label='Return to projects archive.' className="relative block w-full h-full py-4">
+					{`<- ${project.title }`}
+				</a>
 
-					<div>
-						Created: { dateConverter(new Date(Date.parse(project.created))) }<br/>
-						Updated: { dateConverter(new Date(Date.parse(project.updated)))}<br/>
-						<a href={ project.githubURL } target="_blank" aria-label="Link to project repo.">Repo</a><br/>
-						<a href={ project.website } target="_blank" aria-label="Link to project website.">Project site</a>
+				<div className="flex flex-col md:flex-row-reverse">
+					<div className="flex flex-col w-full md:w-1/3 md:ml-4">
+						<ProjectInfoSection title='created'>
+							{dateConverter(new Date(Date.parse(project.created)))}
+						</ProjectInfoSection>
+
+						<ProjectInfoSection title='updated'>
+							{dateConverter(new Date(Date.parse(project.updated)))}
+						</ProjectInfoSection>
+
+						{(project.taxonomies?.length > 0) &&
+							<ProjectInfoSection title='taxonomies'>
+								<div className="flex flex-row flex-wrap gap-x-2">
+									{project.taxonomies.map((taxonomy: taxonomy) => {
+										return (
+											<span key={taxonomy.title} className="">
+												{taxonomy.title}
+											</span>
+										)
+									})}
+								</div>
+							</ProjectInfoSection>
+						}
+
+						{project.githubURL &&
+							<ProjectInfoSection title='repo'>
+								<div className="flex flex-row gap-x-2 items-center">
+									<IoGitMerge className="h-4 w-4 min-w-[1rem] min-h-[1rem]" />
+									<a href={project.githubURL} target="_blank" aria-label="Link to project repo.">
+										{project.githubURL.split('https://')[1]}
+									</a>
+								</div>
+							</ProjectInfoSection>
+						}
+						
+						{project.website &&
+							<ProjectInfoSection title='site'>
+								<div className="flex flex-row gap-x-2 items-center">
+									<FaLink className="h-4 w-4 min-w-[1rem] min-h-[1rem]" />
+									<a href={project.website} target="_blank" aria-label="Link to project website.">
+										{project.website}
+									</a>
+								</div>
+							</ProjectInfoSection>
+						}
+
+						<ProjectInfoSection title='built by'>
+							<div className="flex flex-row gap-x-2 items-center">
+								<FaGithub className="h-4 w-4 min-w-[1rem] min-h-[1rem]" />
+								<a href={UserData.socials[0]?.url} className="">
+									{UserData.name}
+								</a>
+							</div>
+						</ProjectInfoSection>
 					</div>
 
-					{( project.about && 
-						<MarkdownRenderer>{project.about}</MarkdownRenderer>
-					)}
-					{( !project.about &&
-						<NoteCard markdown={`Uh Oh! This project doesn't have any documentation yet. Don't worry, I'm working on it. Take a look around the rest of the site in the meantime, or reach out to inquire more about this project.`} />
-					)}
+					<div className="flex flex-col md:w-2/3 md:mr-4">
+						{(project.about &&
+							<MarkdownRenderer>{project.about}</MarkdownRenderer>
+						)}
+						{(!project.about &&
+							<NoteCard markdown={`Uh Oh! This project doesn't have any documentation yet. Don't worry, I'm working on it. Take a look around the rest of the site in the meantime, or reach out to inquire more about this project.`} />
+						)}
+					</div>
 				</div>
+			</div>
+		</div>
+	)
+}
 
-
+function ProjectInfoSection({title,children,className}:{title: string, children?: React.JSX.Element | string, className?: string}) {
+	if (!children) return
+	return (
+		<div className={`${className} flex flex-col border-b border-accent-secondary/40 mb-6 w-full`}>
+			<span className="text-fg/80">{title}</span>
+			<div className="py-2">
+				{children}
 			</div>
 		</div>
 	)
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -7,7 +7,7 @@ import { MarkdownRenderer } from '@/components/MarkdownRenderer';
 
 export default function Footer() {
 	return (
-		<footer className="absolute bottom-0 left-0 right-0 w-full h-auto max-w-screen">
+		<footer className="absolute bottom-0 left-0 right-0 w-full h-auto max-w-screen z-20">
 			<div className="main-padding flex flex-col md:flex-row justify-start gap-4 py-6 flex-wrap">
 
 				<ThemeButton />
